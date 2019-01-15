d84882260af4d3a04c861c480f42fe11063832b5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-15 14:28:39 +0000

    Deploy lerucom/js-homework2 to github.com/lerucom/js-homework2.git:gh-pages

--- js/lib.js
+++ js/lib.js
@@ -1,10 +1,7 @@
 export function calculateDistance(fuelConsumption, amountOfFuel) {
-    let result;
     // условие, если расход топлива > 0 и объем топлива >= 0
     if (fuelConsumption > 0 && amountOfFuel >= 0) {
-        result = Math.round(amountOfFuel * 100 / fuelConsumption);
-    } else {
-        result = 0; // или вернет 0
+        return Math.round(amountOfFuel * 100 / fuelConsumption);
     }
-    return result;
+    return 0;
 }
