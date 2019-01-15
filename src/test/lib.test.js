83ae069fbcc35d50956c9b5d6555ea7df4e46781
student <student@localhost>
2019-01-15 17:39:25 +0300

    Fixed#3

--- src/test/lib.test.js
+++ src/test/lib.test.js
@@ -1,6 +1,6 @@
 import {calculateDistance} from "../js/lib.js";
 
-test('calculate distance that can drive', () => {
+test('With the consumption of "9.9" and the amount of fuel "45.4" - the distance "459"', () => {
     const fuelConsumption = 9.9;
     const amountOfFuel = 45.4;
     const result = calculateDistance(fuelConsumption, amountOfFuel);
@@ -8,7 +8,7 @@ test('calculate distance that can drive', () => {
     expect(result).toBe(459);
 });
 
-test('calculate distance that can drive', () => {
+test('With the consumption of "23.8" and the amount of fuel "15.2" - the distance "0"', () => {
     const fuelConsumption = 23.8;
     const amountOfFuel = 15.2;
     const result = calculateDistance(fuelConsumption, amountOfFuel);
@@ -16,7 +16,7 @@ test('calculate distance that can drive', () => {
     expect(result).toBe(64);
 });
 
-test('calculate distance that can drive', () => {
+test('With the consumption of "0" and the amount of fuel "15.2" - the distance "0"', () => {
     const fuelConsumption = 0;
     const amountOfFuel = 15.2;
     let result = calculateDistance(fuelConsumption, amountOfFuel);
