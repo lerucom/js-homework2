b5c8de96f9fb7ca25508c28a252b499b48a1e283
student <student@localhost>
2019-01-17 10:44:43 +0300

    lib.js edited

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,5 +1,4 @@
 export function calculateDistance(fuelConsumption, amountOfFuel) {
-    // условие, если расход топлива > 0 и объем топлива >= 0
     if (fuelConsumption > 0 && amountOfFuel >= 0) {
         return Math.round(amountOfFuel * 100 / fuelConsumption);
     }
